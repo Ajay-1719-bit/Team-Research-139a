49837a5afe5dd66ff9341e4fe3ea2720cf6878c7
jk24abv <jk24abv@herts.ac.uk>
2024-11-21 15:13:22 +0000

    Histogram Updated on new Data filter

--- main.R
+++ main.R
@@ -1,2 +1,3 @@
 filtered_data <- subset(mydataset, Make == "AUDI" | Make == "BMW")
 
+hist(filtered_data$`CO2 Emissions(g/km)`, col ="blue",border = "black",freq = TRUE, main = "Histogram of Audi and BMW Emission",xlab ="Co2 Emission")
